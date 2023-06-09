7e6d2e49306160c2a0bdeb836833b8a1011b622b
wook888 <guddnr888@gmail.com>
2023-06-10 00:24:39 +0900

    이제 되나요!!!!?

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Detail from "./routes/Detail";
 import Home from "./routes/Home";
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <Route exact path="/hello" element={<h1>Hello</h1>} />
-      <Route exact path="/movie/:id" element={<Detail />} />
-      <Route exact path="/" element={<Home />} />
+    <Router>
+      <Switch>
+        <Route path={process.env.PUBLIC_URL + "/movie/:id"}>
+          <Detail />
+        </Route>
+        <Route exact path={process.env.PUBLIC_URL + "/"}>
+          <Home />
+        </Route>
+        <Route path={process.env.PUBLIC_URL + "/search/:search"}></Route>
+      </Switch>
     </Router>
   );
 }
