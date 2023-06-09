e0821a59502276a82b5fb83b594e51a057c52a12
wook888 <guddnr888@gmail.com>
2023-06-09 22:53:47 +0900

    변경

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 import Detail from "./routes/Detail";
 import Home from "./routes/Home";
 
